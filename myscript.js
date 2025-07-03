377e2205d451e592c1fdd8b3fc4ed5bd55475630
Sarkodie <sarkodiemichael399@gmail.com>
2025-07-03 02:44:20 +0000

    Add start drawing functionality
    Allows user to first set grid size

--- myscript.js
+++ myscript.js
@@ -1,14 +1,30 @@
+const btn = document.querySelector(".btn");
+const dialog = document.querySelector("#myDialog");
+const container = document.querySelector(".container");
+const confirmSize = document.querySelector("#confirm-btn");
+const closs = document.querySelector("#close-btn");
+const input = document.querySelector("#demoInput");
+let gSize = 16;
+
+btn.addEventListener("click",()=> dialog.showModal());
+closs.addEventListener("click", ()=> dialog.close());
+confirmSize.addEventListener("click", ()=> confirmUpdate());
+
+function confirmUpdate(){
+     gSize=input.value;
+     makeGrids(gSize);
+}
 
 function makeGrids(size)
 {
-let container = document.querySelector(".container");
+container.innerHTML = "";
 for (let i = 0; i < size; i++)
 {
   let column = document.createElement("div");
   column.classList.add("column");
   for (let j = 1; j <= size; j++)
   {
-    let row = document.createElement("div");
+    const row = document.createElement("div");
     row.classList.add("row");
     row.style.border = "0.1px solid rgba(0, 0, 0, 0.05)";
     row.style.width = (960/size) + "px";
@@ -21,4 +37,3 @@ for (let i = 0; i < size; i++)
 
 }
 
-makeGrids(100);
